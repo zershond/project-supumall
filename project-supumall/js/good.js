96c6075f49bf934ec041e7d20a396d84f66ad26f
陈志纯 <837561690@qq.com>
2016-09-26 18:02:54 +0800

    2016.9.26

--- project-supumall/js/good.js
+++ project-supumall/js/good.js
@@ -16,6 +16,17 @@ $(function(){
 			$.cookie('autologin','0');
 			window.location.href = 'index.html';
 		})
+		
+		//显示主导航栏
+		$('.nav-allClass').hover(function(event){
+			$('.nav-allClass-menu').show()
+		},function(){
+			
+		})
+		$('.nav-menu-info').mouseleave(function(){
+			$('.nav-allClass-menu').hide()
+		})
+		
 	});
 	
 	//加载脚部
@@ -23,13 +34,16 @@ $(function(){
 		$(this).html(data);
 	});
 	
+	//加载详情页面
+	$('.more-info-container').load('moreInfo.html');
+	
 	
 //	放大镜执行函数
 	$(".bigPic").jqueryzoom({
-		xzoom: 355,//放大区域宽度
-		yzoom: 355,//放大区域高度
+		xzoom: 500,//放大区域宽度
+		yzoom: 500,//放大区域高度
 		preload: 1,//是否显示预加载
-		offset:10,//放大区域偏离小图的距离
+		offset:0,//放大区域偏离小图的距离
 		position: "right",//放大区域显示的位置（left,right）
 		lens:true //是否显示小图上的透明区域
 	});
@@ -39,6 +53,26 @@ $(function(){
 		$('img',$('.bigPic')).attr('src',src).attr('jqimg',src);
 	})
 	
+	//切换城市
+	$('#province').change(function(){
+		var province = $('option:selected', $(this)).val();
+		var city = $.get('data/city.json',function(data){
+			$('#city').html('<option value="">请选择城市</option>');
+			console.log(data);
+			var len = data.city[province].length;
+			for(var i = 0; i < len; i++){
+				var optionObj = $('#city option:eq(0)').clone();
+				$(optionObj).val(data.city[province][i]).html(data.city[province][i]).appendTo('#city');				
+			}
+			
+			
+		},'json');
+		if(province == '北京'){
+			$('#freightCount').html('免运费    预计正常3-5个工作日送达');
+		}else{
+			$('#freightCount').html('免运费    预计正常3-5个工作日送达');
+		}
+	})
 	
 	//点击增加购买数量
 	$('.count-opa').click(function(){
@@ -51,30 +85,47 @@ $(function(){
 		}
 	});
 	
+	//点击添加购物车
+	$('#add-mallCar').click(function(event){
+		$('#add-mallCar-img').css({
+			left: '712px',
+			top: '634px',
+			display: 'block',
+		}).animate({
+			left: 0,
+			top: 0,
+		},function(){
+			$(this).hide()
+		})
+	})
+	
+	//商品详情与评论之间切换
+	$('.more-info-title li').click(function(){
+		$('.more-info-title li').removeClass('active');
+		$(this).addClass('active');
+		var src = $(this).attr('a');
+		console.log(src);
+		$('.more-info-container').load(src);
+	})
+	
 	
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	//滚动条滚动时
+$(window).scroll(function(){
+	var top = $(window).scrollTop();
+	if(top >= 800){
+		$('.more-info-title').css({
+			position: 'fixed',
+			top: 0
+		})
+	}else{
+		$('.more-info-title').css({
+			position: '',
+			top: 0
+		})
+	}
+})
 
 
 function process(){
---
+++ project-supumall/js/list.js
@@ -0,0 +1,96 @@
+
+
+
+$(function(){
+		//加载头部
+	$('#header').load('heater.html',function(){
+		$('.nav-allClass-menu').hide();
+		$('.adv-right').hide();
+		
+		if($.cookie('autologin') == '1'){
+			process();
+		}
+		
+		//退出按钮绑定执行函数
+		$("#quit").click(function(){
+			$.cookie('autologin','0');
+			window.location.href = 'index.html';
+		})
+		
+		//显示主导航栏
+		$('.nav-allClass').hover(function(event){
+			$('.nav-allClass-menu').show()
+		},function(){
+			
+		})
+		$('.nav-menu-info').mouseleave(function(){
+			$('.nav-allClass-menu').hide()
+		})
+		
+	});
+	
+		
+
+	//加载脚部
+	$('#footer').load('footer.html',function(data){
+		$(this).html(data);
+	});
+	
+	//console.log(123)
+	getPage(1);
+	
+//	翻页按钮特效
+	$('.pager').click(function(){
+		$('.pager').removeClass('active');
+		var index;
+		if($(this).html() == 1 || $(this).html() == '首页' ){
+			$('.pager').eq(0).addClass('active');
+			$('.pager').eq(1).addClass('active');
+			index = 1;
+		}else if($(this).html() == 3 || $(this).html() == '尾页'){
+			$('.pager').eq(3).addClass('active');
+			$('.pager').eq(4).addClass('active');
+			index = 3;
+		}else{
+			$(this).addClass('active');
+			index = 2;
+		}
+		getPage(index);
+	})
+	
+	
+
+	
+	
+});
+
+
+
+
+function getPage(index){
+	var page = 'listDataPage' + index;
+	var str = '<div class="list-item"><img src="img/list/list1-1.jpg"/><div class="title">方广 乖乖小馒头香蕉味15g</div><div class="price"><span>市场价：￥1.8</span><h3>速普特惠价：￥1</h3></div></div>'
+	$('.listContainer').html(str)
+	$.get('data/list.json',function(data){
+		var len = data[page].length;
+		for(var i = 1; i< len; i++){
+			var obj = $('.list-item:eq(0)').clone();
+			obj.find('img').attr('src',data[page][i]['imgSrc']);
+			obj.find('.title').html(data[page][i]['title']);
+			obj.find('.price').find('span').html(data[page][i]['price1']);
+			obj.find('.price').find('h3').html(data[page][i]['price2']);
+			obj.appendTo('.listContainer');
+		}
+	})
+}
+
+
+
+
+function process(){
+	var account = $.cookie('account');
+	//Hi 130*****331 欢迎来到速普商城！ [ 退出 ] 
+	var str = 'hi  <a href="#">';
+	str = str + account + '</a> 欢迎来到速普商城！ [ <a id="quit" href="#">退出</a> ]';
+	$('.header-title span').eq(0).html(str);
+}
